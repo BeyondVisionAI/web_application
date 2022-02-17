e58ec66694a31efeb4b8ff2d9471384588d31cc1
Alex Gaignard <alex.gaignard@epitech.eu>
2022-02-16 17:16:29 -0800

    Fixed auth not checking password and added pretty emails

--- Backend/Controllers/User/authMiddleware.js
+++ Backend/Controllers/User/authMiddleware.js
@@ -12,3 +12,15 @@ exports.authenticateUser = function(req, res, next) {
         return res.status(401).send(Errors.USER_NOT_LOGIN);
     }
 }
+
+exports.onlyForUnAuthenticatedUser = function (req, res, next) {
+    const userJWT = req.cookies.token;
+    try {
+        const user = jwt.verify(userJWT, process.env.JWT_SECRETKEY);
+        req.user = user;
+        return res.status(401).send(Errors.USER_ALREADY_LOGIN);
+    } catch (err) {
+        res.clearCookie("token");
+        next();
+    }
+}
--- Backend/routes/User.js
+++ Backend/routes/User.js
@@ -3,15 +3,15 @@ module.exports = function(app) {
     const User = require('../Controllers/User/User')
     const Middleware = require('../Controllers/User/authMiddleware')
 
-    app.post('/user/register', Auth.register)
+    app.post('/user/register', Middleware.onlyForUnAuthenticatedUser, Auth.register)
 
-    app.post('/user/login', Auth.login)
+    app.post('/user/login', Middleware.onlyForUnAuthenticatedUser, Auth.login)
     
     app.post('/user/logout', Middleware.authenticateUser, Auth.logout)
 
     app.post('/user/verifyEmail', Auth.verifyEmail)
 
-    app.post('/user/askForPasswordChange', Auth.askForPasswordChange)
+    app.post('/user/askForPasswordChange', Middleware.onlyForUnAuthenticatedUser, Auth.askForPasswordChange)
     
     app.post('/user/changePassword', Auth.changePassword)
 
