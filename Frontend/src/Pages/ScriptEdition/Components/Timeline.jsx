c70f8a4f97d95a90a687ca1f7f9326c397ee5b99
tim <timothe.fertin@epitech.eu>
2022-03-23 10:31:28 +0100

    comment deletion done (:warning: I still need to implement the owner-check so the ctm only pops if the user is the owner)

--- Frontend/src/Pages/ScriptEdition/Components/Timeline.jsx
+++ Frontend/src/Pages/ScriptEdition/Components/Timeline.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import { ContextMenu, ContextMenuTrigger, MenuItem } from 'react-contextmenu';
+import '../react-contextmenu.css';
 
 const Timeline = ({replicas, onReplicaSelection}) => {
     console.log(JSON.stringify(replicas));
     const replicaLine = replicas.map((replica, index) => {
         return (
-            <button className='h-fit w-fit bg-green-700 p-4 rounded focus:outline-none focus:border focus:border-blue-600' key={index}
-                onClick={() => onReplicaSelection(replica._id)}>
-                <p>{replica.content}</p>
-            </button>
+            <ContextMenuTrigger id='replica-ctm'>
+                <button className='h-fit w-fit bg-green-700 p-4 rounded focus:outline-none focus:border focus:border-blue-600' key={index}
+                    onClick={() => onReplicaSelection(replica._id)}>
+                    <p>{replica.content}</p>
+                </button>
+            </ContextMenuTrigger>
         )
     })
 
     return (
-        <div className='flex flex-row justify-between items-center
-        w-full h-full bg-green-400 rounded-b-3xl opacity-50 shadow-lg'>
-            {replicaLine}
-        </div>
+        <>
+            <div className='flex flex-row justify-between items-center
+            w-full h-full bg-green-400 rounded-b-3xl opacity-50 shadow-lg'>
+                {replicaLine}
+            </div>
+
+            <ContextMenu id='replica-ctm'>
+                <MenuItem onClick={() => console.log("handle deletion of clicked replica...")}>
+                    Supprimer ...
+                </MenuItem>
+            </ContextMenu>
+        </>
     )
 }
 
