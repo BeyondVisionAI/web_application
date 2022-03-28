f0f7da4de34bff85a40a6258395275fb512a2bb8
Clain Dimitri <47006656+Dimitri-CLAIN@users.noreply.github.com>
2022-03-27 23:42:16 -0400

    Add collaboration check

--- Backend/routes/Media/Image.js
+++ Backend/routes/Media/Image.js
@@ -1,13 +1,15 @@
 module.exports = function(app) {
     const image = require('../../Controllers/Media/Image/Image');
     const authMiddleware = require('../../Controllers/User/authMiddleware');
+    const collabMiddleware = require('../../Controllers/User/collabMiddleware');
 
     app.post('/images',
         authMiddleware.authenticateUser,
         image.createImage
     );
-    app.get('/images/:id',
+    app.get('/images/:projectId/:id',
         authMiddleware.authenticateUser,
+        collabMiddleware.isCollab,
         image.getImage
     );
 }
