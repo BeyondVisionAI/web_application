6e6de0be7bd76cddde5a5857db196d8d98dd7daf
Clain Dimitri <dimitri-jean.clain@epitech.eu>
2022-06-14 17:34:29 +0400

    Update Video model on sns update

--- Backend/Controllers/Media/Video/Video.js
+++ Backend/Controllers/Media/Video/Video.js
@@ -36,47 +36,27 @@ exports.createVideo = async function(req, res)
     }
 }
 
-// exports.putVideoUrl = async function(req, res)
-// {
-//     try {
-//         const video = await Video.findByIdAndUpdate(req.params.id, req.body, {returnDocument: 'after'});
-
-//         return res.status(200).send(video);
-//     } catch (err) {
-//         console.log("Video->updateUrl: " + err);
-
-//         return res.status(500).send(Errors.INTERNAL_ERROR);
-//     }
-// }
-
 exports.snsEndpoint = async function(req, res)
 {
-    // try {
+    try {
         let chunks = [];
         req.on('data', function (chunk) {
             chunks.push(chunk);
         });
         req.on('end', function () {
             let message = JSON.parse(chunks.join(''));
-            console.log('Nous avons un call message');
-            console.log(message);
-
             let body = JSON.parse(message.Message);
-            console.log(body);
-            console.log(body.hlsUrl ? body.hlsUrl : body.status);
+            let video;
+            if (body.status) {
+                video = await Video.findByIdAndUpdate(body.srcVideo.split('.')[0], {status: body.status}, {returnDocument: 'after'});
+            } else if (body.hlsUrl) {
+                video = await Video.findByIdAndUpdate(body.srcVideo.split('.')[0], {url: body.hlsUrl, status: body.workflowStatus}, {returnDocument: 'after'});
+            }
+            res.end();
         });
-    //     let video;
+    } catch (err) {
+        console.log("Update Video data from SNS notification: " + err);
 
-    //     if (req.body.status) {
-    //         video = await Video.findByIdAndUpdate(req.params.id, {status: req.body.status}, {returnDocument: 'after'});
-    //     } else if (req.body.hlsUrl) {
-    //         video = await Video.findByIdAndUpdate(req.params.id, {url: req.body.hlsUrl, status: req.body.workflowStatus}, {returnDocument: 'after'});
-    //     }
-    //     console.log(video)
-    //     return res.status(200).send(video);
-    // } catch (err) {
-    //     console.log("Update Video data from SNS notification: " + err);
-
-    //     return res.status(500).send(Errors.INTERNAL_ERROR);
-    // }
+        return res.status(500).send(Errors.INTERNAL_ERROR);
+    }
 }
