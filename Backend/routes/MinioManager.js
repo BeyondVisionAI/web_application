9b47ba6dfee4ed3a34f1f4371ae519fc9aafbd9a
Léo Ménard <71273462+softhy85@users.noreply.github.com>
2022-10-06 17:18:46 +0200

    (Missing from the last push) Change a bit the file routes of MinioManager (style change)

--- Backend/routes/MinioManager.js
+++ Backend/routes/MinioManager.js
@@ -1,6 +1,3 @@
-const authMiddleware = require("../Controllers/User/authMiddleware");
-const collabMiddleware = require("../Controllers/Collaboration/collabMiddleware");
-const MinioManager = require("../Controllers/MinioManager/MinioManager");
 module.exports = function (app) {
     const authMiddleware = require("../Controllers/User/authMiddleware");
     const collabMiddleware = require("../Controllers/Collaboration/collabMiddleware");
