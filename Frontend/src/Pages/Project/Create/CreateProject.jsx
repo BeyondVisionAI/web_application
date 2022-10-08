3caee152a28a1092bbb9a796924e4a2e373f5883
Alex Gaignard <alexgaignard@pop-os.localdomain>
2022-10-08 14:10:35 +0200

    Fixed some errors that were on dev

--- Frontend/src/Pages/Project/Create/CreateProject.jsx
+++ Frontend/src/Pages/Project/Create/CreateProject.jsx
@@ -79,6 +79,7 @@ export default function CreateProject({ show, onHide }) {
             handleChange('id', projectResponse.data._id);
             uploadMedia();
             history.push(`/project/${projectResponse.data._id}`);
+            onHide()
         } catch (error) {
             console.error(error);
         }
--- Frontend/src/Pages/Project/Create/ThumbnailDisplay.jsx
+++ Frontend/src/Pages/Project/Create/ThumbnailDisplay.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const ThumbnailDisplay = ({thumbnail, removeThumbnail}) => {
+    console.log("🚀 ~ file: ThumbnailDisplay.jsx ~ line 6 ~ ThumbnailDisplay ~ thumbnail", thumbnail)
+    console.log("🚀 ~ file: ThumbnailDisplay.jsx ~ line 11 ~ useEffect ~ thumbnail.type", thumbnail.type)
     const [isHovered, setIsHovered] = useState(false)
+    const [image, setImage] = useState(null)
 
+    useEffect(() => {
+        if (thumbnail?.type?.includes('image')) {
+            var reader = new FileReader();
+            reader.onload = function(){
+                setImage(reader.result)
+            };
+            reader.readAsDataURL(thumbnail)
+        } else {
+            setImage(thumbnail)
+        }
+    }, []);
+
+    if (!image) return <h1>Loading...</h1>
     return ( 
         <div
         onClick={removeThumbnail}
@@ -14,7 +30,7 @@ const ThumbnailDisplay = ({thumbnail, removeThumbnail}) => {
         >
             {isHovered && <div className="bg-red-500/50 absolute flex w-full h-full"></div>}
             {isHovered && <FontAwesomeIcon className="absolute text-4xl text-white" icon={faTrash} />}
-            <img className="object-scale-down w-full h-full" src={ thumbnail } alt="Thumbnail"></img>
+            <img className="object-scale-down w-full h-full" src={ image } alt="Thumbnail"></img>
         </div>
      );
 }
