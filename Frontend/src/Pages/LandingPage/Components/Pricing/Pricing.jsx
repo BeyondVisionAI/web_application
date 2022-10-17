c46e7768d29cc5f77bc19aeb13bb288a937ca1c6
tim <timothe.fertin@epitech.eu>
2022-10-18 00:33:49 +0200

    changed the premium price from 95 a year to 8 a month

--- Frontend/src/Pages/LandingPage/Components/Pricing/Pricing.jsx
+++ Frontend/src/Pages/LandingPage/Components/Pricing/Pricing.jsx
@@ -7,7 +7,7 @@ const Pricing = () => {
         <div href="pricing" id="pricing" className="pricing-container">
             <PricingElement animation="fade-right" delay="500" isMainElement={false} buttonMessage="FREE" title="Free Plan" listElements={["Up to 5 projects", "5 minutes long video maximum", , "Watermark Beyond Vision"]} />
             <PricingElement animation="zoom-in" delay="0" isMainElement={true} buttonMessage="€5.99/month" title="Basic Plan" listElements={["Up to 10 projects", "Collaborative projects", "Export the finished videos without watermark"]} />
-            <PricingElement animation="fade-left" delay="500" isMainElement={false} buttonMessage="€95/year" title="Pro Plan" listElements={["Unlimited projects", "Voice shop advantages", "Certified audiodescription review", "Export the finished videos in 4K"]} />
+            <PricingElement animation="fade-left" delay="500" isMainElement={false} buttonMessage="€7.99/month" title="Pro Plan" listElements={["Unlimited projects", "Voice shop advantages", "Certified audiodescription review", "Export the finished videos in 4K"]} />
         </div>
     );
 }
