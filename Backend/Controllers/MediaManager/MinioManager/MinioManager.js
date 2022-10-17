f77bfe83c6d752856813ed20f35f2eecba841c7e
Léo Ménard <71273462+softhy85@users.noreply.github.com>
2022-10-17 20:38:46 +0200

    typo bug in minio

--- Backend/Controllers/MediaManager/MinioManager/MinioManager.js
+++ Backend/Controllers/MediaManager/MinioManager/MinioManager.js
@@ -7,7 +7,7 @@ const minioClient = new Minio.Client({
     endPoint: process.env.MINIO_ENDPOINT,
     port: parseInt(process.env.MINIO_PORT),
     useSSL: true,
-    accesKey: process.env.MINIO_ACCESS_KEY,
+    accessKey: process.env.MINIO_ACCESS_KEY,
     secretKey: process.env.MINIO_SECRET_KEY
 });
 
