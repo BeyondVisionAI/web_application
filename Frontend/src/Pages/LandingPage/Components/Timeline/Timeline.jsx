b6dfa00d156542259f009d32ad68cae0aa9870f7
MinMatth-Magi <desrues.matthieu@gmail.com>
2022-11-17 10:28:09 +0100

    Removed comments and fixed typos

--- Frontend/src/Pages/LandingPage/Components/Timeline/Timeline.jsx
+++ Frontend/src/Pages/LandingPage/Components/Timeline/Timeline.jsx
@@ -11,8 +11,8 @@ const Timeline = () => {
   ]
   return (
     <div href="timeline" id="timeline" className="timeline-body">
-      {/* <!-- The Timeline --> */}
-        <h1 style={{marginBottom: '2vh'}}>{t('header')}</h1>
+      <h1 style={{marginBottom: '2vh'}}>{t('header')}</h1>
+
       <ul className="timeline">
         {timeline.map((milestone => (
           <li>
@@ -31,66 +31,6 @@ const Timeline = () => {
             </div>
           </li>
         )))}
-      {/*
-        <li>
-          <div className="direction-l">
-            <div className="flag-wrapper">
-              <span className="flag wbg">September 2020</span>
-              <span className="time-wrapper hide">
-                <span className="time">2011 - 2013</span>
-              </span>
-            </div>
-            <div className="desc">
-              Definition of the project
-            </div>
-          </div>
-        </li>
-
-        <li>
-          <div className="direction-l">
-            <div className="flag-wrapper">
-              <span className="flag wbg">February 2021</span>
-              <span className="time-wrapper hide">
-                <span className="time">2011 - 2013</span>
-              </span>
-            </div>
-            <div className="desc">
-              Design and Tests
-            </div>
-          </div>
-        </li>
-
-        <li>
-          <div className="direction-l">
-            <div className="flag-wrapper">
-              <span className="flag wbg">May 2021</span>
-              <span className="time-wrapper hide">
-                <span className="time">2011 - 2013</span>
-              </span>
-            </div>
-            <div className="desc">
-              Start of the development of the software
-            </div>
-          </div>
-        </li>
-  */}
-
-        {/* <!-- Item Focsued --> */}
-        {/*
-        <li>
-          <div className="direction-l">
-            <div className="flag-wrapper">
-              <span className="flag xl">May 2022 </span>
-              <span className="time-wrapper hide">
-                <span className="time">2011 - 2013</span>
-              </span>
-            </div>
-            <div className="desc">
-              {t('May_22.description')}
-            </div>
-          </div>
-        </li>
-        */}
       </ul>
     </div>
   );
