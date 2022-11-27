1f3cb9fef88524609ac708a933f180995a3e1e85
Clain Dimitri <dimitri-jean.clain@epitech.eu>
2022-11-27 15:49:16 +0100

    Fix reload page on add collaborators

--- Frontend/src/GenericComponents/InputWithLabel/CollaboratorInput.jsx
+++ Frontend/src/GenericComponents/InputWithLabel/CollaboratorInput.jsx
@@ -31,7 +31,8 @@ function CollaboratorInput({ defaultValue, collaborators, setCollaborators }) {
         }
     }
 
-    const addCollaborator = async () => {
+    const addCollaborator = async (e) => {
+        e.preventDefault();
         try {
             if (collaborators.find(collaborator => collaborator.user.email === newCollaborator)) {
                 toast.warning("This collaborator is already added");
@@ -78,7 +79,7 @@ function CollaboratorInput({ defaultValue, collaborators, setCollaborators }) {
                 />
 
                 <div className="flex mt-2 mx-3">
-                    <button onClick={() => addCollaborator()}> {/* TODO: On click reload page*/}
+                    <button onClick={(e) => addCollaborator(e)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/><circle cx="8.5" cy="7" r="4"/><line x1="20" y1="8" x2="20" y2="14"/><line x1="23" y1="11" x2="17" y2="11"/></svg>
                     </button>
                 </div>
